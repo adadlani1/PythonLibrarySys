6bd83684cc5aa74e8a9869cfa5e0a7f67e8264e6
adadlani1 <45290647+adadlani1@users.noreply.github.com>
2018-12-01 00:15:46 +0000

    Update bookreturn.py

--- bookreturn.py
+++ bookreturn.py
@@ -3,6 +3,7 @@ import os
 ''' imported the date time module so that the Due column in the text file can be updated.'''
 f = open("Database.txt" , "r")
 y = open("Logfile.txt" , "a")
+w = open("Weedingfile.txt", "r")
 ''' The code below is for the dates of today and the future so that it can be updated on the database and the logfile'''
 tdelta = datetime.timedelta(days = 21)
 tday = datetime.date.today()
@@ -28,6 +29,7 @@ while True:
  if 999<inputID<10000:
     a = input("Which book would you like to returnor press 'q' to return to menu? Please enter the id number:")
     databaseLines = f.readlines()
+    weedingLines = w.readlines()
     for i in range (1,24):
         currentLine = databaseLines[i]
         bookID = currentLine[0:2]
@@ -36,12 +38,16 @@ while True:
             checkAvailable = checkAvailable[-2]
             if checkAvailable == "8" or "9":
                 databaseLines[i] = bookID+'\tBook_'+bookID+'\t\tAuthor_'+bookID+'\tY\t\t-\n'
+                weedingLines[i] = bookID+'\tBook_'+bookID+'\t\tAuthor_'+bookID+'\t'+today+'\n'
                 print(databaseLines[0])
                 print(databaseLines[i])
                 f.close()
+                w.close()
                 f = open("Database.txt" , "w")
                 f.writelines(databaseLines)
-                y.write('\n'+bookID+'\tBook_'+bookID+'\t\tAuthor_'+bookID+'\tN\t\t' +today+ '\t\t%d\n' % (inputID))                
+                y.write('\n'+bookID+'\tBook_'+bookID+'\t\tAuthor_'+bookID+'\tN\t\t' +today+ '\t\t%d\n' % (inputID))
+                w = open("Weedingfile.txt", "w")
+                w.writelines(weedingLines)
                 break
             else:
                 print("That book is already available. Please try again later...")
